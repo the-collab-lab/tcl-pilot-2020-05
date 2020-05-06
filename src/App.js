ae896fb7e01ba4f0c4a690c151aafc2c45d8c111
Ashling <hennessyaisling9@gmail.com>
2020-05-05 17:58:33 -0700

    Create Main and Footer.js file and complete CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Main />
+      
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Main = () =>{
+    return (<section className="main"></section>)
+}
+
+export default Main;
