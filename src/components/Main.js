562bdc3bd00e414aeabfc404f60d6f418d3597e4
Amber <ajiles91@gmail.com>
2020-05-08 00:00:12 -0400

    restructured to resolve merge conflicts

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Map from '../components/Map';
 
 
 const Main = () =>{
-    return (<section className="main"></section>)
+    return (
+        <section className="main">
+            <Map />
+        </section>
+    )
 }
 
 export default Main;
