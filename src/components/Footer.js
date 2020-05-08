81941500a5e842d3211ea41d01c906a359cab70e
Matthew J. Hughes <mjhughes707@gmail.com>
2020-05-07 21:08:45 -0700

    resolved height issue caused by importing Map

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,10 @@ const Footer= () => {
         <footer className= "footer">
             
         <button>Share Location</button>
-        <div>
         <p><strong> Click "Share Location"</strong> to discover interesting landmarks around you from Wikipedia</p>
         <p><strong>Tap on a marker</strong> on the map and more information about it will show up in this space. Have fun!</p>
         <p>Note: Your location is never stored in the app or on our servers!</p>
         <p><strong> What's near me?</strong> Looks and works best when installed on your phone as an app.</p>
-        </div>
        
 
         </footer>
