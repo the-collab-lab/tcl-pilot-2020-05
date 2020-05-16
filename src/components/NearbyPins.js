5aaa0dc3e8ab92d5d4ef2278cdb198ce5034d50b
jbatara <jennifer.batara.dev@gmail.com>
2020-05-16 16:18:33 -0700

    Parse json into dict, create local state, useEffect
    Parse json response into usable dict
    
    Added local state for nearbyPlaces
    
    Use useEffect to set initial nearbyPlaces state

--- src/components/NearbyPins.js
+++ src/components/NearbyPins.js
@@ -1,38 +1,67 @@
-import React from 'react';
-import Pins from './Pins';
+import React, { useEffect, useState } from 'react';
+import Pin from './Pins';
 
 
-const fetchNearbyPlacesPromise = (lat, lng) => {
-    return new Promise((resolve, reject) => {
-        let request = new XMLHttpRequest();
-        let url = `https://segdeha.com/api/nearby.php?lat=${lat}&lng=${lng}`
+//creates promise to fetch data
+const fetchNearbyPlacesPromise = async (lat, lng) => {
+    let url = `https://segdeha.com/api/nearby.php?lat=${lat}&lng=${lng}`
+    let response = await fetch(url);
 
-        request.onload = () => {
-            {this.status === 200? resolve(request.response) : reject(Error(request.statusText))}
+    let output;
+    if(response.ok){
+        output = await response.json();
+
+    } else {
+        console.log("Error" + response.status);
+        output = { };
+    }
+    return output;
+}
+
+//executes the fetch, and formats data for consumption by Pin component
+async function fetchNearbyPlaces(lat, lng) {
+    let response = await fetchNearbyPlacesPromise(lat,lng)
+
+    let output = [];
+    if(response){
+        const nearbyPlaces = await response.pages;
+        output = await nearbyPlaces.map( (place) => {
+            const lat = place.coordinates[0].lat
+            const lon = place.coordinates[0].lon
+            const description = place.description;
+            const title = place.title;
+            const image = place.thumbnail.source;
+            const dict = {lat, lon, description, title, image};
+            console.log(dict);
+            return dict;
+        });
         }
-        request.open("GET", url, true);
-        request.send();
-    });
-}
-
-async const fetchNearbyPlaces  = (lat, lng) => {
-    response = await fetchNearbyPlacesPromise(lat,lng)
-        .then(res => JSON.parse(res));
-    print(response)
+    // } else {
+    // // might want to differentiate between error and no outputs in the future. Currently will render same thing.
+    //     output = [];
     
+   return output;
 }
-
 
 // Looks for all nearby places, generates a list, and maps child Pin components for each place
 
-export default NearbyPins = ({lat, lng}) => {
-    let output = []
-    console.log(fetchNearbyPlaces(lat,lng));
+const NearbyPins = ({lat, lng}) => {
+    const [nearbyPlaces, setNearbyPlaces] = useState([]);
+
+    useEffect(() => {
+        let fetchNearby = fetchNearbyPlaces(lat, lng);
+        setNearbyPlaces(fetchNearby);
+    }, [])
+
     return (
         <React.Fragment>
-            {output.map((place, index) => <Pin key={index} name={ } text={ } image={ } />)}
+            {(nearbyPlaces.map(
+                    ({lat,lon,title,description,image}, index) => <Pin key={index} lat={lat} lon={lon} title={title} description={description} image={image} />))
+            }
+
         </ React.Fragment>
     )
 
 }
 
+export default NearbyPins;
