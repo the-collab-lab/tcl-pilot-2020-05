bd2b81dd2e1ae969cf9ff1d497b33dc80f94b3ad
jbatara <jennifer.batara.dev@gmail.com>
2020-05-16 15:14:14 -0700

    Add fetchNearbyPlacesPromise and fetchNearbyPlaces
    fecthNearbyPlacesPromise makes the request to the API and returns
    a promise. Helper function for fetchNearbyPlaces
    
    fetchNeabyPlaces to return a list of Pin components for the map
    component To do:
    1. parse JSON
    2. explore object for wanted information
    3. create data model for ease of output to Pin component

--- src/components/NearbyPins.js
+++ src/components/NearbyPins.js
@@ -2,6 +2,27 @@ import React from 'react';
 import Pins from './Pins';
 
 
+const fetchNearbyPlacesPromise = (lat, lng) => {
+    return new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        let url = `https://segdeha.com/api/nearby.php?lat=${lat}&lng=${lng}`
+
+        request.onload = () => {
+            {this.status === 200? resolve(request.response) : reject(Error(request.statusText))}
+        }
+        request.open("GET", url, true);
+        request.send();
+    });
+}
+
+async const fetchNearbyPlaces  = (lat, lng) => {
+    response = await fetchNearbyPlacesPromise(lat,lng)
+        .then(res => JSON.parse(res));
+    print(response)
+    
+}
+
+
 // Looks for all nearby places, generates a list, and maps child Pin components for each place
 
 export default NearbyPins = ({lat, lng}) => {
