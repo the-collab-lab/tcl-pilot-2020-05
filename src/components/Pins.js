4129a07bbe73bd3d507ebc5601673f77deead387
jbatara <jennifer.batara.dev@gmail.com>
2020-05-16 16:51:43 -0700

    Add inline styling to Pin component

--- src/components/Pins.js
+++ src/components/Pins.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Pin = ({title, description, image}) => {
     const DEFAULT_IMAGE = "../../public/img/default-thumb.png"
     return (
-        <div>
+        <div style={{height:"25px",width:"25px", fontSize:"10%"}}>
             {title?<h1>{title}</h1>:<h1>Unkown</h1>}
             {image? (<img src={image} height="10" width="10"></img>):(<img src={DEFAULT_IMAGE} height="10" width="10"></img>)}
             {description? <p>Description: {description}</p>:<p>No Description</p>}
