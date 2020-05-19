d758db100258bac7aa44cdfee55a3d0b84602efb
Ashling <hennessyaisling9@gmail.com>
2020-05-18 19:25:00 -0700

    pin CSS changes

--- src/components/Pins.js
+++ src/components/Pins.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Pin = ({title, description, image}) => {
     const DEFAULT_IMAGE = "../../public/img/default-thumb.png"
     return (
-        <div style={{height:"25px",width:"25px", fontSize:"10%"}}>
+        <div className= "pins" style={{height:"25px",width:"25px", fontSize:"10%"}}>
             {title?<h1>{title}</h1>:<h1>Unkown</h1>}
-            {image? (<img src={image} height="10" width="10"></img>):(<img src={DEFAULT_IMAGE} height="10" width="10"></img>)}
+            {image? (<img src={image} height="50" width="50"></img>):(<img src={DEFAULT_IMAGE} height="50" width="50"></img>)}
             {description? <p>Description: {description}</p>:<p>No Description</p>}
         </div>
 
