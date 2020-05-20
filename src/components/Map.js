842d05822edfc6fafab091afae198a3710c3b281
Matthew J. Hughes <mjhughes707@gmail.com>
2020-05-19 17:44:11 -0700

    removed default props and allowed for dynamic data

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,20 +4,13 @@ import MyLocation from "./MyLocation";
 
 function Map(props) {
   const { data } = props;
-  const defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33,
-    },
-    zoom: 11,
-  };
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyA_jF-TPUl8qTMZ3BKFTrFOolH9wR7NOz4" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
+        center={data.center}
+        zoom={data.zoom}
       >
         <MyLocation lat={data.center.lat} lng={data.center.lng} />
       </GoogleMapReact>
