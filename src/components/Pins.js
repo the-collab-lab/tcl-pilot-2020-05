6878c64f19e5e8a60e119f5585b08aa7c3a62121
jbatara <jennifer.batara.dev@gmail.com>
2020-05-19 17:48:39 -0700

    Change to self closing img tag

--- src/components/Pins.js
+++ src/components/Pins.js
@@ -4,7 +4,7 @@ const Pin = ({image}) => {
     const DEFAULT_IMAGE = "/img/default-thumb.png"
     return (
         <div className= "pins">
-            {image? (<img src={image} alt={"venues across location"}></img>):(<img src={DEFAULT_IMAGE} alt={"venues across location"}></img>)}
+            {image? (<img src={image} alt={"venues across location"} />):(<img src={DEFAULT_IMAGE} alt={"venues across location"} />)}
         </div>
 
     )
