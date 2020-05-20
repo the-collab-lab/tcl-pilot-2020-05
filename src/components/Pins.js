54aa4f084d593d15b79490e2846fdaa52ba0bc9c
jbatara <jennifer.batara.dev@gmail.com>
2020-05-19 17:37:29 -0700

    Fix pin img resizing past boarder, fix default img path
    Image was resizing out of pin because absolute width and height
    were passed in inline styles, but the Pin component was dynamically
    resizing on window size. Changed css of image to inherit from class
    pin.
    
    Default image was not rendering because used releative path,
    instead of public facing path.

--- src/components/Pins.js
+++ src/components/Pins.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Pin = ({image}) => {
-    const DEFAULT_IMAGE = "../../public/img/default-thumb.png"
+    const DEFAULT_IMAGE = "/img/default-thumb.png"
     return (
         <div className= "pins">
-            {image? (<img src={image} alt={"venues across location"} height="50" width="50"></img>):(<img src={DEFAULT_IMAGE} alt={"venues across location"} height="50" width="50"></img>)}
+            {image? (<img src={image} alt={"venues across location"}></img>):(<img src={DEFAULT_IMAGE} alt={"venues across location"}></img>)}
         </div>
 
     )
