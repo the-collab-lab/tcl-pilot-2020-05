b69d3eb44b8212e8ff44fee070c858b25d928bc1
Ashling <hennessyaisling9@gmail.com>
2020-05-20 18:28:05 -0700

    Created two constants DefaultFooter and DescriptionFooter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,37 @@
 import React from "react";
+import PinDescription from "PinDescription";
 
 
 
-const Footer= () => {
-    return (
-        <footer className= "footer">
+
+const Footer= ({title, description, image, displayDescription}) => {
+     
+
+    const descriptionFooter = <PinDescription title = {title} description ={description} image = {image}/>
+  
+
+
+    const defaultFooter = ( 
+    
+    <footer className= "footer">
             
-        <button>Share Location</button>
-        <p><strong> Click "Share Location"</strong> to discover interesting landmarks around you from Wikipedia</p>
-        <p><strong>Tap on a marker</strong> on the map and more information about it will show up in this space. Have fun!</p>
-        <p>Note: Your location is never stored in the app or on our servers!</p>
-        <p><strong> What's near me?</strong> Looks and works best when installed on your phone as an app.</p>
+    <button>Share Location</button>
+    <p><strong> Click "Share Location"</strong> to discover interesting landmarks around you from Wikipedia</p>
+    <p><strong>Tap on a marker</strong> on the map and more information about it will show up in this space. Have fun!</p>
+    <p>Note: Your location is never stored in the app or on our servers!</p>
+    <p><strong> What's near me?</strong> Looks and works best when installed on your phone as an app.</p>
+   
+
+    </footer>
+    
+    )
+
+    return (
+        { displayDescription? descriptionFooter : defaultFooter }
        
 
-        </footer>
     )
 }
+
 
 export default Footer;
