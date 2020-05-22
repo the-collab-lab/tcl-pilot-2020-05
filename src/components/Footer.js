0ec246f0f150a04ffca0b3372e52b9a5d1e576c5
jbatara <jennifer.batara.dev@gmail.com>
2020-05-21 17:18:09 -0700

    Move footer tag to surround turnary in return, change defaultFooter to React.Fragment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,25 +9,19 @@ const Footer= ({title, description, image, displayDescription}) => {
   
 
     const defaultFooter = (
-    
-    <footer className= "footer">
-            
-    <button>Share Location</button>
-    <p><strong> Click "Share Location"</strong> to discover interesting landmarks around you from Wikipedia</p>
-    <p><strong>Tap on a marker</strong> on the map and more information about it will show up in this space. Have fun!</p>
-    <p>Note: Your location is never stored in the app or on our servers!</p>
-    <p><strong> What's near me?</strong> Looks and works best when installed on your phone as an app.</p>
-   
-
-    </footer>
-   
-    
+        <React.Fragment>      
+            <button>Share Location</button>
+            <p><strong> Click "Share Location"</strong> to discover interesting landmarks around you from Wikipedia</p>
+            <p><strong>Tap on a marker</strong> on the map and more information about it will show up in this space. Have fun!</p>
+            <p>Note: Your location is never stored in the app or on our servers!</p>
+            <p><strong> What's near me?</strong> Looks and works best when installed on your phone as an app.</p>
+        </React.Fragment>  
     )
     
 
     return (
       
-        <p>{ displayDescription? descriptionFooter : defaultFooter }</p>
+        <footer className= "footer">{ displayDescription? descriptionFooter : defaultFooter }</footer>
        
 
     )
