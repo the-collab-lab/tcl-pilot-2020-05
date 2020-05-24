74d8bb42908f0e11513b348d85155ef527094c5b
Matthew J. Hughes <mjhughes707@gmail.com>
2020-05-24 16:28:49 -0700

    removed comments and unnecessary spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,34 +5,27 @@ const Footer = ({ handleClick, marker }) => {
   return (
     <footer className="footer">
       <h1>
-        {" "}
         {marker ? (
           <button disabled> Location Shared </button>
         ) : (
-          <button onClick={handleClick}>Share Location?</button>
+          <button onClick={handleClick}> Share Location? </button>
         )}
       </h1>
 
       <div>
         <p>
-          {" "}
           <strong> Click "Share Location" </strong> to discover interesting
           landmarks around you from Wikipedia
         </p>
         <p>
-          {" "}
           <strong> Tap on a marker </strong> on the map and more information
           about it will show up in this space. Have fun!
         </p>
+        <p>Note: Your location is never stored in the app or on our servers!</p>
         <p>
-          {" "}
-          Note: Your location is never stored in the app or on our servers!{" "}
-        </p>{" "}
-        <p>
-          {" "}
           <strong> What 's near me?</strong> Looks and works best when installed
           on your phone as an app.
-        </p>{" "}
+        </p>
       </div>
     </footer>
   );
--- src/components/MyLocation.js
+++ src/components/MyLocation.js
@@ -1,6 +1,4 @@
 import React from "react";
 import "../App.css";
 
-//const MyLocation = () => <span className="MyLocation" role="img" aria-label="My location">⚫</span>;
-
 export default () => <div className="MyLocation" />;
