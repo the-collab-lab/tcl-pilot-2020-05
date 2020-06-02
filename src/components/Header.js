ddea1c27d132537302dfa7c128297b6e25d86d5e
Matthew J. Hughes <mjhughes707@gmail.com>
2020-06-01 20:04:37 -0700

    added modal w/ conditional display to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import SettingsModal from "./SettingsModal";
 
-const Header = () => {
-  return <header className="header">What's near me?</header>;
+const Header = ({ displayModal, openModal, closeModal }) => {
+  return (
+    <Fragment>
+      {displayModal && <SettingsModal closeModal={closeModal} />}
+
+      {/* onClick will later be moved to the forthcoming "settings" icon */}
+      <header onClick={openModal} className="header">
+        What's near me?
+      </header>
+    </Fragment>
+  );
+};
+
+Header.propTypes = {
+  displayModal: PropTypes.bool.isRequired,
+  openModal: PropTypes.func.isRequired,
+  closeModal: PropTypes.func.isRequired,
 };
 
 export default Header;
