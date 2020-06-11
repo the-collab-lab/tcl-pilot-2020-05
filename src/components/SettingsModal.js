a460951b99beb1622f38660df7e0c3f9ce73373a
Amber <ajiles91@gmail.com>
2020-06-11 19:41:24 -0400

    added emoji and displaying slider value in setings modal

--- src/components/SettingsModal.js
+++ src/components/SettingsModal.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 export default function SettingsModal({ setDisplayModal }) {
+  const [sliderValue, setSliderValue] = useState(15);
+
+  const handleSlide = (event) => {
+    const { value } = event.target;
+    setSliderValue(value);
+
+    console.log("value from slider:", event.target.value);
+  };
+
   return (
     <div id="modal" className="modal-container">
       <section className="modal-content">
@@ -12,11 +21,17 @@ export default function SettingsModal({ setDisplayModal }) {
           className="modal-close-button"
         />
         <h1 className="modal-header">Settings</h1>
+
         <div className="modal-range-container">
           <label htmlFor="delay-slider">
             Set how frequently the app will fetch new places. Values are in
             seconds.
           </label>
+          <div className="modal-range-pic-container">
+            <img src="./img/slider-walker.png" alt="walker emoji" />
+            <img src="./img/slider-bike.png" alt="bike emoji" />
+            <img src="./img/slider-car.png" alt="car-emoji" />
+          </div>
           <input
             type="range"
             id="delay-slider"
@@ -26,7 +41,9 @@ export default function SettingsModal({ setDisplayModal }) {
             max="30"
             defaultValue="15"
             step="5"
+            onInput={(event) => handleSlide(event)}
           />
+          <div className="slider-value">{sliderValue}</div>
         </div>
       </section>
     </div>
