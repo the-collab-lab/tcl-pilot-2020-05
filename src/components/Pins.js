269e2cd6eb4a7ae07fee42e2d7f5d66d4067c2b6
Ashling <hennessyaisling9@gmail.com>
2020-06-23 17:03:09 -0700

    change to alt attribute

--- src/components/Pins.js
+++ src/components/Pins.js
@@ -10,9 +10,9 @@ const Pin = ({ image, place, setCurrentPin, setDisplayInformation }) => {
   return (
     <div onClick={onClick} className="pins">
       {image ? (
-        <img src={image} alt={"venues across location"} />
+        <img src={image} alt={"local venue"} />
       ) : (
-        <img src={DEFAULT_IMAGE} alt={"venues across location"} />
+        <img src={DEFAULT_IMAGE} alt={"local venue"} />
       )}
     </div>
   );
